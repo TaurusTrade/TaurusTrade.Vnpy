b9f72a2851b8b0a5d1ef3ee1e24ccd6f277e5119
Holbrook <wanghaikuo@tsinghua.org.cn>
2021-03-09 16:53:12 +0800

    import

---
+++ src/TaurusTrade.Vnpy/VnpyCtaStrategyEngine.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaurusTrade.Vnpy
+{
+    public class VnpyCtaStrategyEngine
+    {
+        //python only, needn't RPC?
+
+
+
+        public Dictionary<string,VnpyCtaStrategy> Strategies { get; set; }      //# strategy_name: strategy
+
+        public VnpyCtaStrategyEngine()
+        {
+
+        }
+
+        public void OnTick(Tick tick)
+        {
+
+        }
+
+        def process_tick_event(self, event: Event):
+        """"""
+        tick = event.data
+
+        strategies = self.symbol_strategy_map [tick.vt_symbol]
+        if not strategies:
+            return
+
+        self.check_stop_order(tick)
+
+        for strategy in strategies:
+            if strategy.inited:
+                self.call_strategy_func(strategy, strategy.on_tick, tick)
+
+    def process_order_event(self, event: Event):
+        """"""
+        order = event.data
+
+        self.offset_converter.update_order(order)
+
+        strategy = self.orderid_strategy_map.get(order.vt_orderid, None)
+        if not strategy:
+            return
+
+        # Remove vt_orderid if order is no longer active.
+        vt_orderids = self.strategy_orderid_map[strategy.strategy_name]
+        if order.vt_orderid in vt_orderids and not order.is_active():
+            vt_orderids.remove(order.vt_orderid)
+
+        # For server stop order, call strategy on_stop_order function
+        if order.type == OrderType.STOP:
+            so = StopOrder(
+                vt_symbol= order.vt_symbol,
+                direction= order.direction,
+                offset= order.offset,
+                price= order.price,
+                volume= order.volume,
+                stop_orderid= order.vt_orderid,
+                strategy_name= strategy.strategy_name,
+                status= STOP_STATUS_MAP[order.status],
+                vt_orderids=[order.vt_orderid],
+            )
+            self.call_strategy_func(strategy, strategy.on_stop_order, so)
+
+# Call strategy on_order function
+        self.call_strategy_func(strategy, strategy.on_order, order)
+
+    def process_trade_event(self, event: Event):
+        """"""
+        trade = event.data
+
+        # Filter duplicate trade push
+        if trade.vt_tradeid in self.vt_tradeids:
+            return
+        self.vt_tradeids.add(trade.vt_tradeid)
+
+        self.offset_converter.update_trade(trade)
+
+        strategy = self.orderid_strategy_map.get(trade.vt_orderid, None)
+        if not strategy:
+            return
+
+        # Update strategy pos before calling on_trade method
+        if trade.direction == Direction.LONG:
+            strategy.pos += trade.volume
+        else:
+            strategy.pos -= trade.volume
+
+        self.call_strategy_func(strategy, strategy.on_trade, trade)
+
+        # Sync strategy variables to data file
+        self.sync_strategy_data(strategy)
+
+# Update GUI
+        self.put_strategy_event(strategy)
+
+    def process_position_event(self, event: Event):
+        """"""
+        position = event.data
+
+        self.offset_converter.update_position(position)
+
+
+        public void Close()
+        {
+
+        }
+    }
+}
---
+++ src/TaurusTrade.Vnpy/VnpyServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaurusTrade.Vnpy
+{
+    public class VnpyServer
+    {
+        public string ResponseAddress { get; set; }
+        public string PublishAddress { get; set; }
+
+        public void AddGateway(VnpyGateway gateway)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+
+        public void Stop()
+        {
+
+        }
+    }
+}
